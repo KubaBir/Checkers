55365877b4a214c86349270fa447b8253830ec41
Kuba Birecki <kubabir@outlook.com>
2022-12-30 11:54:47 +0100

    sync

--- src/main.cpp
+++ src/main.cpp
@@ -10,18 +10,6 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    // Board board;
-
-    // board.add_piece(unique_ptr<Piece>(new Pawn(1, 8, WHITE)));
-    // // board.add_piece(unique_ptr<Piece>(new Pawn(2, 7, WHITE)));
-    // // board.add_piece(unique_ptr<Piece>(new Pawn(3, 7, WHITE)));
-    // // board.add_piece(unique_ptr<Piece>(new Pawn(4, 7, WHITE)));
-
-    // board.attempt_promote();
-    // // board.get_pieces();
-    // for (const auto& piece : board.get_pieces()) {
-    //     cout << piece->get_is_queen() << piece->get_pos().x << endl;
-    // }
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
     sf::RenderWindow window(sf::VideoMode(BOARD_SIZE + SIDESCREAN_SIZE, BOARD_SIZE), "Checkers", sf::Style::Close, settings);
